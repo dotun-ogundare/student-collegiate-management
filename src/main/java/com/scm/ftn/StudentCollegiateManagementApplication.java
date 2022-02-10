c9f8b7932950a064ed39de0b16dcfe2a778c8066
dotun <dotun.ogundare@gmail.com>
2022-02-10 14:41:53 +0100

    Initial commit, as generated from spring initializer

---
+++ src/main/java/com/scm/ftn/StudentCollegiateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.scm.ftn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCollegiateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCollegiateManagementApplication.class, args);
+	}
+
+}
