f5cdabcdaa0bed6c0dad9e5c618cc1c0f6574786
dotun <dotun.ogundare@gmail.com>
2022-02-12 20:36:25 +0100

    created frontend and a simple api to transmit data from backend to frontend

---
+++ src/main/java/com/scm/ftn/student/Gender.java
@@ -0,0 +1,7 @@
+package com.scm.ftn.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/scm/ftn/student/Student.java
@@ -0,0 +1,17 @@
+package com.scm.ftn.student;
+
+import lombok.*;
+
+//@Data
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
