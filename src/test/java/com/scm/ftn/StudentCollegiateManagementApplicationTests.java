82372009ec7b60ec6a595d401fbd6efba4e705fd
dotun <dotun.ogundare@gmail.com>
2022-02-26 21:49:08 +0100

    this particular test should fail

--- src/test/java/com/scm/ftn/StudentCollegiateManagementApplicationTests.java
+++ src/test/java/com/scm/ftn/StudentCollegiateManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.scm.ftn;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentCollegiateManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("oops! test failed categorically");
 	}
 
 }
