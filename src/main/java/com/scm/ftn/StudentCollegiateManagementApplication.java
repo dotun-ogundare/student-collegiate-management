9472f9d7eece9bc48af747fc4ac0defd7736319b
dotun <dotun.ogundare@gmail.com>
2022-03-02 03:05:48 +0100

    line comment added to ensure pull request is triggered

--- src/main/java/com/scm/ftn/StudentCollegiateManagementApplication.java
+++ src/main/java/com/scm/ftn/StudentCollegiateManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentCollegiateManagementApplication {
-
+	//to ensure pull request is triggered
 	public static void main(String[] args) {
 		SpringApplication.run(StudentCollegiateManagementApplication.class, args);
 	}
